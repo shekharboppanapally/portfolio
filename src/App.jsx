680d532fe2ecbf2689f05ffda9f687c16af1e7cc
h547708 <boppanapally.shekhar@honeywell.com>
2025-05-07 16:43:55 +0530

    Fix GitHub Pages deployment: update homepage URL and change to HashRouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyles from './styles/GlobalStyles';
 import { theme } from './styles/theme';
